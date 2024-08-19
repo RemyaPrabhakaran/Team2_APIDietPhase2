4fae011512dfa559e627188cb707ec17a4bd6cc2
rashmikula1 <124323549+rashmikula1@users.noreply.github.com>
2024-08-19 00:05:57 -0400

    Update Patient_Post.java
    Resolving conflicts

--- src/test/java/payload/Patient_Post.java
+++ src/test/java/payload/Patient_Post.java
@@ -1,54 +1,35 @@
 package payload;
 
 public class Patient_Post {
-	public String FirstName;
-    public String getFirstName() {
-		return FirstName;
-	}
-	public void setFirstName(String firstName) {
-		FirstName = firstName;
-	}
-	public String getLastName() {
-		return LastName;
-	}
-	public void setLastName(String lastName) {
-		LastName = lastName;
-	}
-	public long getContactNumber() {
-		return ContactNumber;
-	}
-	public void setContactNumber(long contactNumber) {
-		ContactNumber = contactNumber;
-	}
-	public String getEmail() {
-		return Email;
-	}
-	public void setEmail(String email) {
-		Email = email;
-	}
-	public String getAllergy() {
-		return Allergy;
-	}
-	public void setAllergy(String allergy) {
-		Allergy = allergy;
-	}
-	public String getFoodCategory() {
-		return FoodCategory;
-	}
-	public void setFoodCategory(String foodCategory) {
-		FoodCategory = foodCategory;
-	}
-	public String getDateOfBirth() {
-		return DateOfBirth;
-	}
-	public void setDateOfBirth(String dateOfBirth) {
-		DateOfBirth = dateOfBirth;
-	}
+	
+   
 	public String LastName;
     public long ContactNumber;
     public String Email;
     public String Allergy;
     public String FoodCategory;
     public String DateOfBirth;
+
+	public  String FirstName;
+	
+    public  String FoodPreference;
+   
+    public  String CuisineCategory;
+
+    
+  
+    public Patient_Post(String firstName, String lastName, String contactNumber, String email, String allergy, String foodPreference, String dateOfBirth, String cuisineCategory
+) {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.ContactNumber = Long.parseLong(contactNumber); // Convert String to long
+        this.Email = email;
+        this.Allergy = allergy;
+        this.FoodPreference = foodPreference;
+        this.DateOfBirth = dateOfBirth;
+        this.CuisineCategory= cuisineCategory;
+
+    }
+    
 
 }
